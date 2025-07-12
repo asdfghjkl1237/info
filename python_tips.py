20e6699061318413df965f91c1fb5ec0e5ef19a7
asdfghjkl1237 <kov.tangome@gmail.com>
2025-07-12 17:36:22 +0300

    Create python_tips.py

---
+++ python_tips.py
@@ -0,0 +1,5 @@
+# creating venv
+# [1] python3 -m venv {{ name_of_the_env }}
+# [2]> pip install ipykernel
+# [3]> python3 -m ipykernel install --user --name={{ name_of_the_env }}
+# [4] need totally reload VSC 
