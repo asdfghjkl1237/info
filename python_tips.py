a2bb2ffa048219df85ca315e516836f0120085ea
asdfghjkl1237 <kov.tangome@gmail.com>
2026-04-07 01:04:42 +0300

    Update python_tips.py

--- python_tips.py
+++ python_tips.py
@@ -3,3 +3,9 @@
 # [2]> pip install ipykernel
 # [3]> python3 -m ipykernel install --user --name={{ name_of_the_env }}
 # [4] need totally reload VSC 
+
+
+# also need to activate created new one env
+# and there do this:
+# [2]> pip install ipykernel
+# [3]> python3 -m ipykernel install --user --name={{ name_of_the_env }}
